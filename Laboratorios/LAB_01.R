94e17c915456d26292a044724975d89458603d1f
Amanda Rosales <amanda.rosalessa@uanl.edu.mx>
2022-02-03 12:59:22 -0600

    Update

--- Laboratorios/LAB_01.R
+++ Laboratorios/LAB_01.R
@@ -1,3 +1,47 @@
-#Amanada
-#02/02/2022
+# Amanda
+# 02/02/2022
 # Laboratorio 1
+
+
+# Primer contacto con la consola de R -------------------------------------
+
+
+# Gastos totales
+300 + 240 + 1527 + 400 + 1500 + 1833
+
+celular <- 300
+celular
+transporte <- 240
+comestibles <- 1527
+gimnasio <- 400
+alquiler <- 1500
+otros <- 1833
+
+total.gastos <- celular + transporte + comestibles + gimnasio + alquiler + otros
+
+# gastos en 5 meses
+
+total.gastos * 5
+
+# gastos en 10 meses
+
+total.gastos * 10
+
+
+# Nombre de los objetos ---------------------------------------------------
+
+yo_uso_guion_bajo <- 5
+otras.personas.usan.punto <- 10
+OtrosUsanMayusculas <- 1
+
+# no puede iniciar con un número
+# no puede iniciar con guión bajo
+# no puede contener coma
+# no puede contener espacios 
+
+
+# Funciones ---------------------------------------------------------------
+
+# valor absoluto (absolute valve)
+abs (10)
+abs (-4)
