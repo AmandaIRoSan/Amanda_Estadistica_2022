55b8225c1fb4df73aaebddf876cc8f206f010222
PC Amanda SC <amanda.rosalessa@uanl.edu.mx>
2022-02-09 08:57:52 -0600

    Update

---
+++ CLASES/Clase_S4_D1.R
@@ -0,0 +1,31 @@
+# AMANDA
+# 09/02/2022
+# semanda 4 Sla de computo
+
+
+# Importar datos ----------------------------------------------------------
+
+vivero <- read.csv("CLASES/BD_vivero.csv", header = TRUE)
+View(vivero)
+
+
+# Estadística descriptiva -------------------------------------------------
+
+# Medidas de tendencia central
+
+mean(vivero$IE)
+median(vivero$IE)
+range(vivero$IE) 
+fivenum(vivero$IE) 
+ 
+# representación gráfica
+ 
+boxplot(vivero$IE, horizontal = TRUE, col = "blue") 
+hist(vivero$IE, ylim = c(0,12)) 
+
+# tabla de  frecuencia
+ 
+?table 
+table(vivero$IE)
+ftable(vivero$IE) 
+ 
