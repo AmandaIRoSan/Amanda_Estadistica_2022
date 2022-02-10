5fbc9da3427999c2c3cfd4df30e068ad82d6b862
Amanda Rosales <amanda.rosalessa@uanl.edu.mx>
2022-02-10 12:03:50 -0600

    Update

--- Laboratorios/LAB_02.R
+++ Laboratorios/LAB_02.R
@@ -6,14 +6,28 @@
 # Importar datos ----------------------------------------------------------
 
 getwd()
+setwd("C:/Users/isa_r/OneDrive - Universidad Autonoma de Nuevo León/2do. SEMESTRE/PRINCIPIOS DE ESTADÍSTICA/Amanda_Estadistica_2022/Laboratorios")
+trees <- read.csv("BDH_1.csv", header = T)
+head(trees)
+
+# Ingresar datos directo en la consola
+
+dbh <- c(16.5, 25.3, 22.1, 17.2, 16.1, 8.1, 34.3, 5.4, 5.7, 11.2, 24.1, 14.5, 7.7, 15.6, 15.9, 10, 17.5, 20.5, 7.8, 27.3, 9.7, 6.5, 23.4, 8.2, 28.5, 10.4, 11.5, 14.3, 17.2, 16.8)
 
 # Datos de URL no seguras (http)
 
 prof.url <- "http://www.profepa.gob.mx/innovaportal/file/7635/1/accionesInspeccionvsrm.csv"
-a.profepa <- read.csv(prof.url)
+Profepa <- read.csv(prof.url)
+head(Profepa)
+prof_url_2 <- paste0("http://www.profepa.gob.mx/innovaportal/",
+                     "file/7635/1/accionesInspeccionvsrm.csv")
+profepa2 <- read.csv(prof_url_2)
+head(profepa2)
 
 # Datos de URL seguras (https): Dropbox y Github
 
+library(repmis) # VERIFICAR
+conjunto <- source()
 
 # Operaciones con la base de datos ----------------------------------------
 
