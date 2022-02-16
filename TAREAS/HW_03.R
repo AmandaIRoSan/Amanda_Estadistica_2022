f4aa14c2ccde717a81ab1249ff4af4b3c79d4374
Amanda Rosales <amanda.rosalessa@uanl.edu.mx>
2022-02-16 13:53:40 -0600

    Update

---
+++ TAREAS/HW_03.R
@@ -0,0 +1,50 @@
+# Amanda
+# 16/02/2022
+# Tarea 3: Medidas de tendencia central
+
+
+# Problemas ---------------------------------------------------------------
+
+# Problema 1
+Xs <- c(6, 4, 1, 3)
+Ys <- c(1, 3, 4, 2)
+sum(Xs)
+sum(Ys)
+prod(Xs)
+prod(Ys)
+sum(Xs, Ys)
+prod(Xs, Ys)
+
+# Problema 2
+GrupoA <- c(80, 90, 90, 100)
+GrupoB <- c(60, 65, 65, 70, 70, 70, 75, 75, 80, 80, 80, 80, 80, 85, 100)
+median(GrupoA)
+median(GrupoB)
+
+# Problema 3
+promedio80 <- c(87, 72, 85, 75)
+median(promedio80)
+
+# Problema 4
+110/50
+
+# Problema 5
+
+
+# Problema 6
+set <-c(2, 2, 3, 6, 10)
+# a)
+median(set)
+mean(set)
+mode <- function(set) {return(as.numeric(names(which.max(table(set)))))}
+
+# b)
+sum(set, cinco)
+cinco <- c(5, 5, 5, 5, 5)
+
+# c)
+
+# d)
+prod(set, cinco)
+
+# Problema 7
